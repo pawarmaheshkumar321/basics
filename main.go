74c051acb75ae780ca986d5c80b79ce13a3632ac
maheshkumar <11630290+pawarmaheshkumar321@users.noreply.github.com>
2025-06-14 22:00:35 +0530

    second commit

--- main.go
+++ main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"fmt"
-	//"net/http"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Hello, Go Standard Library")
+	resp, err := http.Get("http://tilltoss.net:5000/tahsil/getsettings2")
+
+	if err != nil {
+		fmt.Println("The error is", err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("This is response from GET request Status", resp.Status)
+	fmt.Println("This is response from GET request StatusCode", resp.StatusCode)
+	fmt.Println("This is response from GET request Body", resp.Body)
 }
